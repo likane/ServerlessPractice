8c170ba008e2a34baae90a072acec387051e88a6
Louis kane <lkane02@yahoo.com>
2020-07-21 21:40:17 -0700

    creating a new market app

--- agora/src/App.js
+++ agora/src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./App.css";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { API, graphqlOperation, Amplify, Auth, Hub } from 'aws-amplify';
 import { AmplifyTheme, Authenticator, SignOut } from 'aws-amplify-react';
 import { AmplifySignOut, Greetings} from '@aws-amplify/ui-react';
 
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import MarketPage from  './pages/MarketPage';
+
+
+
+import Navbar from  './components/Navbar';
+
+
 import awsconfig from './aws-exports';
+
+export const UserContext = React.createContext()
 
 Amplify.configure(awsconfig);
 
@@ -41,12 +53,32 @@ class App extends React.Component {
     }
   }
 
+  handleSignout = async () => {
+    try {
+      await Auth.signOut();
+    } catch (err) {
+      console.error('Error Signing out user', err);
+    }
+  }
+
   render() {
     const {user} = this.state;
-    return !user ? (<Authenticator theme={theme} /> ) : <div> 
-      <AmplifySignOut />
-      App
-      </div>;
+    return !user ? (<Authenticator theme={theme} /> ) : (
+      <UserContext.Provider value={{ user }}>
+      <Router>
+      
+      <>
+      <Navbar user={user} handleSignout={this.handleSignout} />
+        <div className="app-container">
+        {/* <AmplifySignOut /> */}
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route exact path="/markets/:marketId" component={({match}) => <MarketPage marketId={match.params.marketId}/>} />
+        </div>
+      </>
+      </Router>
+      </UserContext.Provider>
+      )
   }
 }
 
--- agora/src/components/MarketList.js
+++ agora/src/components/MarketList.js
@@ -1,8 +1,78 @@
 import React from "react";
-// import { Loading, Card, Icon, Tag } from "element-react";
+import {Connect} from 'aws-amplify-react'
+import { Loading, Card, Icon, Tag } from "element-react";
+import {graphqlOperation} from 'aws-amplify'
+import {listMarkets} from '../graphql/queries'
+import {onCreateMarket} from '../graphql/subscriptions'
+import Error from './Error'
+import {Link} from 'react-router-dom'
+import MarketPage from "../pages/MarketPage";
 
 const MarketList = () => {
-  return <div>MarketList</div>;
+  const onNewMarket = (prevQuery, newData) => {
+    let updatedQuery = {...prevQuery};
+    const updatedMarketList = [
+      newData.onCreateMarket,
+      ...prevQuery.listMarkets.items
+    ]
+    updatedQuery.listMarkets.item = updatedMarketList;
+    return updatedQuery;
+  }
+  return (
+    <Connect
+      query={graphqlOperation(listMarkets)}
+      subscription={graphqlOperation(onCreateMarket)}
+      onSubscriptionMsg={onNewMarket}
+    >
+      {({data, loading, errors }) => {
+        if (errors.length > 0) return <Error errors={errors} />
+        if (loading || !data.listMarkets) return <Loading fullscreen={true} />
+        return(
+          <>
+          <h2 className="header">
+            <img src="https://icon.now.sh/store_mall_directory/527FFF" alt="Store Icon" className="large-icon" />
+            Markets
+          </h2>
+            {data.listMarkets.items.map(market => (
+              <div key={market.id} className="my-2">
+                <Card
+                  bodyStyle={{
+                    padding: "0.7em",
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                  }}
+                  >
+                      <div>
+                        <span className="flex">
+                            <Link className="link" to={`/markets/${market.id}`}>
+                              {market.name}
+                            </Link>
+                            <span style={{color: 'var(--darkAmazonOrange'}}>
+                              {/* {market.products.items.length} */}
+                            </span>
+                            <img src="https://icon.now.sh/shopping_cart/f60" alt="Shopping Cart" />
+                        </span>
+                        <div style={{color: "var(--lightSquidInk"}}>
+                          {market.owner}
+                        </div>
+                        </div>
+                          <div>
+                        {market.tags && market.tags.map(tag => (
+                          <Tag key={tag} type="danger" className="mx-1">
+                            {tag}
+                          </Tag>
+                        ))}
+                      </div>
+                </Card>
+              </div>
+            ))}
+          </>
+        )
+      }}
+    </Connect>
+
+  )
 };
 
 export default MarketList;
